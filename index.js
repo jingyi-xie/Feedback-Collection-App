47993d81b29ff20aaca4df0932757ad7dc2186f6
jx95 <jx95@duke.edu>
2020-06-15 12:57:05 -0400

    finished OAuth

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express"); // import express (common js modules)
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session"); // get access to cookie
+const passport = require("passport"); // tell passport to use cookie
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hello: "world" });
-}); // route handler of GET method
+app.use(
+  cookieSession({ maxAge: 30 * 2400 * 60 * 60 * 1000, keys: [keys.cookieKey] })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); // tell Node to listen on which port
