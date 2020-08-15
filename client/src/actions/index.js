e793c6e5f19073c5fa28c09d94f5047b666e6449
jx95 <jx95@duke.edu>
2020-08-15 14:01:09 -0400

    finished webhook

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,3 +13,9 @@ export const handleToken = (token) => async (dispatch) => {
   // shortcut: also return user model
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const submitSurvey = (values, history) => async (dispatch) => {
+  const res = await axios.post("/api/surveys", values);
+  history.push("/surveys");
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
