d7b8b07794a146299046579e1067e0016710a93a
jx95 <jx95@duke.edu>
2020-08-15 15:07:26 -0400

    finished SurveyList

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 //action creator, return a function, make a req, and after the req is
 //completed, do the dispatch action
 export const fetchUser = () => async (dispatch) => {
@@ -18,4 +18,9 @@ export const submitSurvey = (values, history) => async (dispatch) => {
   const res = await axios.post("/api/surveys", values);
   history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async (dispatch) => {
+  const res = await axios.get("/api/surveys");
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
